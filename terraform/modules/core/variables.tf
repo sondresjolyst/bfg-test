fc0a5e3f31a434c6b4a845b5c5b3162c14b1ac7e
sondresjolyst <sonyslyst@gmail.com>
2024-02-21 12:15:52 +0100

    default variables

--- terraform/modules/core/variables.tf
+++ terraform/modules/core/variables.tf
@@ -2,3 +2,15 @@ variable "environment" {
   description = "The name of the environment to create the resources for."
   type        = string
 }
+
+variable "resource_group_name" {
+  description = "The name of the resource group to create the resources in."
+  type        = string
+  default     = "rg-testing-sosj"
+}
+
+variable "location" {
+  description = "The location to create the resources in."
+  type        = string
+  default     = "westeurope"
+}
